9789912ff1deeef6b1e1c9ea5087c15fa9123eae
beautea37 <beautea37@naver.com>
2023-08-27 23:55:24 +0900

    4 commit

--- 4/simple_middleware.js
+++ 4/simple_middleware.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require('express')
 const app = express()
-const PORT = 3000;
-
-//1번 로직
-app.use((req, res, next)=>{
-    console.log('1 Time: ', Date.now())
-    next()
+const PORT = 3000
+// --- 1번로직
+app.use((req, res, next)=>{  
+  console.log('1 Time:', Date.now())
+  next()
 })
-app.use((req, res, next)=>{
-    //2번 로직
-    console.log(`2 Time:`, Date.now())
-    next()
+app.use((req, res, next)=>{  
+  console.log('2 Time:', Date.now())
+  next()
 })
+// --- 2번 로직
 
-app.listen(PORT, ()=> {
-    console.log(`서버 실행. 포트 번호 : ${PORT}`)
+app.listen(PORT, ()=>{
+  console.log(`서버가 생성되었습니다.${PORT}`)
 })
--- 4/simple_morgan.js
+++ 4/simple_morgan.js
@@ -1,20 +1,19 @@
-//express를 통해 app.use 매개변수로 morgan을 넣으면 logging middleware 설정이 가능함.
-
-const express = require('express');
+const express = require('express')
 const app = express()
-const loogger = require('morgan')        //logger에 morgan 모듈 함수 담기
-const PORT = 3000;
-
-app.use(loogger('tiny'));       //매개변수 설정. 로그 형식
-app.use((req, res, next) => {
-    console.log('1 Time: ', Date.now());
-    next();
+const loogger = require('morgan')
+const PORT = 3000
+// --- 1번로직
+app.use(loogger('tiny'))
+app.use((req, res, next)=>{  
+  console.log('1 Time:', Date.now())
+  next()
 })
-app.use((req, res, next) => {
-    console.log('2 Time: ', Date.now());
-    next();
+app.use((req, res, next)=>{  
+  console.log('2 Time:', Date.now())
+  next()
 })
-
-app.listen(PORT, ()=> {
-    console.log(`서버 생성 완료. ${PORT}`);
-});
+// --- 2번 로직
+ 
+app.listen(PORT, ()=>{
+  console.log(`서버가 생성되었습니다.${PORT}`)
+})
